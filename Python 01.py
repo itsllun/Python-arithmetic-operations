0d8a6e1216515de42da7d7ab76260104d80b1a1b
itsllun <88114420+itsllun@users.noreply.github.com>
2021-07-28 17:13:44 -0400

    Add files via upload

---
+++ Python 01.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+'''
+Python 01
+Fatim Majumder
+Level 3
+
+Python arithmetic operations
+'''
+
+a=-2
+b=5.7
+c=1
+
+# equation
+r1 = (-b + ((b**2 - 4*a*c)**(0.5)))/2*a
+r2 = (-b - ((b**2 - 4*a*c)**(0.5)))/2*a
+
+print(r1)
+print(r2)
+
+
+# In[9]:
+
+
+#Testcase2
+a=0.5
+b=-6
+c=18
+
+# equation
+r1 = (-b + ((b**2 - 4*a*c)**(0.5)))/2*a
+r2 = (-b - ((b**2 - 4*a*c)**(0.5)))/2*a
+
+print(r1)
+print(r2) 
+
+
+# In[10]:
+
+
+
+########################################################
+
+a=5
+b=7
+c=21
+d=20
+
+w=int((13*a-1)/5)
+x=int(c/4)
+y=int(d/4)
+z=w+x+y+b+c-2*d
+
+r = z%7
+
+print(r) 
+
+
+# In[ ]:
+
+
+
+
